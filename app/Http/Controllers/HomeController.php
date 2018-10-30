d1e3ef9979bec208910716ba6056ca3289a16f86
3A532072 <dine1138@gmail.com>
2018-10-30 23:58:05 +0800

    Link Button & Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //
+        return view('home');
     }
 
     /**
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,18 +78,9 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <div class="title">About</div>
+                <input type="button" value="About" onclick="self.location.href='{{route('about.index')}}'"/>
+                <input type="button" value="News" onclick="self.location.href='{{route('news.index')}}'"/>
             </div>
         </div>
     </body>
